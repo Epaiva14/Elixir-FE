d4a12a9a2ca086844e7bad2f5d9f48793d61f1cb
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-07-14 22:52:18 -0600

    FEAT: fixed styling for recipe preview card on home page. Trending WIP.

--- src/app/components/recipes/recipeScrollTile.js
+++ src/app/components/recipes/recipeScrollTile.js
@@ -4,13 +4,27 @@ import '../../css/index.css';
 import RecipePreviewContainer from './recipePreviewContainer';
 
 export default function RecipeScrollTile({ recipes }) {
+
+    function twoByTwo(recipes) {
+        
+
+    }
+
     return (
         <>
             <main>
-                {recipes.map(recipe => {
-                    return <RecipePreviewContainer key={recipe._id} recipe={recipe} />
-                })}
+                <div className='tile is-ancestor is-4 scroll-tile'>
+                    <div className='tile is-parent box is-vertical'>
+                        <div className='tile is-parent'>
+
+                            {recipes.map(recipe => {
+                                return <RecipePreviewContainer key={recipe._id} recipe={recipe} />
+                            })}
+
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
-}
+} 
