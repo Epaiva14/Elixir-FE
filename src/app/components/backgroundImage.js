92f0d1335f166e402f4855d52d61ba1935f476d3
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-07-15 16:11:53 -0600

    WIP: Login Style

--- src/app/components/backgroundImage.js
+++ src/app/components/backgroundImage.js
@@ -2,12 +2,14 @@ import elixirBG from '../assets/elixirBG.jpg'
 
 export default function BackgroundImage() {
     const divStyle = {
+
         backgroundImage: `url('https://i.imgur.com/QuJqHP1.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         position: 'relative',
         height: '100vh',
-        zIndex: 1
+        zIndex: 0
+        
     };
 
     return <div style={divStyle}>
