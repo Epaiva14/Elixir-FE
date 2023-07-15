f3da2ef12338f9fa8b99ef683fafc71ce581830b
darkartaudio <mikey@mikeyallred.com>
2023-07-14 20:37:41 -0500

    feat: /search searches for ingredients, recipes, and users

--- src/app/search/searchForm.js
+++ src/app/search/searchForm.js
@@ -5,28 +5,50 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Search() {
-    const resultStyle = {
-        color: 'orange'
-    }
-
     const paramStyle = {
         color: 'green'
     }
 
-    const router = useRouter();
-    const [redirect, setRedirect] = useState(false);
-    const [error, setError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const ingredientStyle = {
+        color: 'blue'
+    }
 
+    const recipeStyle = {
+        color: 'red'
+    }
+
+    const userStyle = {
+        color: 'purple'
+    }
+
+    const router = useRouter();
+    const [error, setError] = useState(false);
     const [query, setQuery] = useState('');
-    const [list, setList] = useState([]);
-    const [ingredients, setIngredients] = useState();
     const [selectedParams, setSelectedParams] = useState([]);
     const [searchResults, setSearchResults] = useState();
+
+    const [searchRedirect, setSearchRedirect] = useState(false);
+    const [recipeRedirect, setRecipeRedirect] = useState(false);
+    const [userRedirect, setUserRedirect] = useState(false);
     
-    const handleChange = (e) => {
+    const [recipesLoading, setRecipesLoading] = useState(true);
+    const [ingredientsLoading, setIngredientsLoading] = useState(true);
+    const [usersLoading, setUsersLoading] = useState(true);
+
+    const [ingredientsList, setIngredientsList] = useState([]);
+    const [recipesList, setRecipesList] = useState([]);
+    const [usersList, setUsersList] = useState([]);
+    
+    const [ingredients, setIngredients] = useState();
+    const [recipes, setRecipes] = useState();
+    const [users, setUsers] = useState();
+
+    const updateIngredientOptions = () => {
+        if(ingredientsLoading) {
+            return setIngredientsList(['Loading...']);
+        }
         const results = ingredients.filter(ingredient => {
-            if (e.target.value === '') return true;
+            if (query === '') return true;
 
             let isAlreadySelected = false;
             selectedParams.forEach(param => {
@@ -34,17 +56,48 @@ export default function Search() {
             });
             if (isAlreadySelected) return false;
             
-            return ingredient.name.toLowerCase().includes(e.target.value.toLowerCase());
+            return ingredient.name.toLowerCase().includes(query.toLowerCase());
         });
 
+        setIngredientsList(results);
+    }
+
+    const updateRecipeOptions = () => {
+        if(recipesLoading) {
+            return setRecipesList(['Loading...']);
+        }
+        const results = recipes.filter(recipe => {
+            if (query === '') return true;
+            return recipe.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+        setRecipesList(results);
+    }
+
+    const updateUserOptions = () => {
+        if(usersLoading) {
+            return setUsersList(['Loading...']);
+        }
+        const results = users.filter(user => {
+            if (query === '') return true;
+            return user.username.toLowerCase().includes(query.toLowerCase());
+        });
+
+        setUsersList(results);
+    }
+    
+    const handleChange = (e) => {
         setQuery(e.target.value);
-        setList(results);
+        updateIngredientOptions();
+        updateRecipeOptions();
+        updateUserOptions();
     }
 
     const addParam = (ingredient) => {
         let newParams = [...selectedParams];
         newParams.push(ingredient);
         setSelectedParams(newParams);
+        updateIngredientOptions();
     }
 
     const removeParam = (ingredient) => {
@@ -53,67 +106,109 @@ export default function Search() {
             return param._id !== ingredient._id;
         });
         setSelectedParams(newParams);
+        updateIngredientOptions();
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/recipes/search`, { selectedParams })
         .then(response => {
             setSearchResults(response.data);
-            setRedirect(true);
+            setSearchRedirect(true);
         })
         .catch(err => {
             setError(true);
         })
     }
-    
+
     useEffect(() => {
-        if (redirect) {
-            // console.log(searchResults);
+        if (searchRedirect) {
             localStorage.setItem('searchResults', JSON.stringify(searchResults.recipes));
-            // router.replace('/search/results', {state: searchResults});
             router.push('/search/results');
         }
 
+        if (recipeRedirect) {
+            localStorage.setItem('recipeId', JSON.stringify(recipeRedirect));
+            router.push('/single/recipe');
+        }
+
+        if (userRedirect) {
+            localStorage.setItem('userId', JSON.stringify(userRedirect));
+            router.push('/single/user');
+        }
+    }, [router, searchResults, searchRedirect, recipeRedirect, userRedirect]);
+    
+    useEffect(() => {
         fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/ingredients`)
         .then((res) => res.json())
         .then((result) => {
             setIngredients(result.ingredients);
-            setIsLoading(false);
+            setIngredientsLoading(false);
         });
-    }, [redirect, router, searchResults]);
+    }, []);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/recipes`)
+        .then((res) => res.json())
+        .then((result) => {
+            setRecipes(result.recipes);
+            setRecipesLoading(false);
+        });
+    }, []);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/users`)
+        .then((res) => res.json())
+        .then((result) => {
+            setUsers(result.users);
+            setUsersLoading(false);
+        });
+    }, []);
+
     
     if (error) {
         return (
             <>
                 <h1>Error</h1>
-                <Link href="/search/mikeysearch">Search Again</Link>
+                <Link href="/search">Search Again</Link>
             </>
         );
     }
-    if (isLoading) return <p>Loading...</p>;
     
-    // if (redirect) {
-    //     console.log(searchResults);
-    //     localStorage.setItem('searchResults', JSON.stringify(searchResults.recipes));
-    //     // router.replace('/search/results', {state: searchResults});
-    //     router.push('/search/results');
-    // }
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <input type="search" value={query} onChange={handleChange} />
                 <button type="submit">Search</button>
             </form>
-            <ul>
+            <ul style={paramStyle}>
                 {(selectedParams === '' ? '' : selectedParams.map(param => {
-                    return <li style={paramStyle} key={param._id}>{param.name} <a onClick={() => {removeParam(param)}}>X</a></li>
+                    return <li key={param._id}>{param.name} <a onClick={() => {
+                        removeParam(param);
+                        updateIngredientOptions();
+                    }}>X</a></li>
                 }))}
             </ul>
-            <ul>
-                {(query === '' ? '' : list.map(ingredient => {
-                    return <li onClick={() => {addParam(ingredient)}} key={ingredient._id}>{ingredient.name}</li>
+            <ul style={ingredientStyle}>
+                {(query === '' ? '' : ingredientsList.map(ingredient => {
+                    return (ingredient === 'Loading...') ? <li key='loading'>Ingredients Loading...</li> : <li key={ingredient._id} onClick={() => {
+                        addParam(ingredient);
+                        updateIngredientOptions();
+                    }}>{ingredient.name}</li>
+                }))}
+            </ul>
+            <ul style={recipeStyle}>
+                {( query === '' ? '' : recipesList.map(recipe => {
+                    return (recipe === 'Loading...') ? <li key='loading'>Recipes Loading...</li> : <li key={recipe._id} onClick={() => {
+                        setRecipeRedirect(recipe._id);
+                    }}>{recipe.name}</li>
+                }))}
+            </ul>
+            <ul style={userStyle}>
+                {( query === '' ? '' : usersList.map(user => {
+                    return (user === 'Loading...') ? <li key='loading'>Users Loading...</li> : <li key={user._id} onClick={() => {
+                        setUserRedirect(user._id);
+                    }}>{user.username}</li>
                 }))}
             </ul>
         </>
