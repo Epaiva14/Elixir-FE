5dce1a69b47be4e4f8e0a1579f3fbc6c3529d20a
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-07-15 20:36:11 -0400

    feat: added css mods to signup page

--- src/app/users/signup/page.js
+++ src/app/users/signup/page.js
@@ -94,72 +94,49 @@ const Signup = () => {
 	// You can have them redirected to profile (your choice)
 
 	return (
-		<div className="container">
-			<br />
-			<br />
-			<br />
-			<br />
-			<div className="row justify-content-center">
-				<div className="col-md-8">
-					<div className="card-group mb-0">
-						<div className="card p-4">
-							<div className="card-body">
-								<form onSubmit={handleSubmit}>
-									<h1>Sign Up</h1>
-									<p className="text-muted">Create an account below to get started</p>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-										<input type="text" className="form-control" placeholder="Full Name" value={fullName} onChange={handleFullName} required />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-										<input type="text" className="form-control" placeholder="Username" value={username} onChange={handleUsername} required />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-mail-forward" aria-hidden="true"></i></span>
-										<input type="email" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-lock"></i></span>
-										<input type="password" className="form-control" placeholder="Password" value={password} onChange={handlePassword} required />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="date" className="form-control" placeholder="Birthdate" value={birthdate} onChange={handleBirthdate} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="Location" value={location} onChange={handleLocation} />
-									</div>
-									<div className="row">
-										<div className="col-6">
-											<button type="submit" className="btn btn-primary px-4">Sign Up</button>
-										</div>
-										<div className="col-6 text-right">
-											<button type="button" className="btn btn-link px-0">Forgot password?</button>
-										</div>
-									</div>
-								</form>
-							</div>
+		<div className="divStyle">
+			<div className='signupForm'>
+				<form onSubmit={handleSubmit}>
+					<h1>Sign Up</h1>
+					<p className="text-muted">Create an account below to get started</p>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
+						<input type="text" className="form-control" placeholder="Full Name" value={fullName} onChange={handleFullName} required />
+					</div>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
+						<input type="text" className="form-control" placeholder="Username" value={username} onChange={handleUsername} required />
+					</div>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-mail-forward" aria-hidden="true"></i></span>
+						<input type="email" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required />
+					</div>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-lock"></i></span>
+						<input type="password" className="form-control" placeholder="Password" value={password} onChange={handlePassword} required />
+					</div>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
+						<input type="date" className="form-control" placeholder="Birthdate" value={birthdate} onChange={handleBirthdate} />
+					</div>
+					<div className="input-group mb-3">
+						<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
+						<input type="text" className="form-control" placeholder="Location" value={location} onChange={handleLocation} />
+					</div>
+					<div className="row">
+						<div className="col-6">
+							<button type="submit" className="btn btn-primary px-4">Sign Up</button>
 						</div>
-						<div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
-							<div className="card-body text-center">
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<div>
-									<h2>Login</h2>
-									<p>Sign In to your account</p>
-									<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
-								</div>
-							</div>
+						<div className="col-6 text-right">
+							<button type="button" className="btn btn-link px-0">Forgot password?</button>
 						</div>
 					</div>
+				</form>
+				<hr />
+				<div>
+					<h2>Already have an account? Click the link below to login.</h2>
+					<p>Sign In to your account</p>
+					<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
 				</div>
 			</div>
 		</div>
