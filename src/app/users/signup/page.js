61c7f4d850873faeec038677a9aeb9dbca2eff05
darkartaudio <mikey@mikeyallred.com>
2023-07-18 12:29:50 -0500

    feat: navbar changes if not logged in, media query gap on navbar fixed, added nav to login and signup

--- src/app/users/signup/page.js
+++ src/app/users/signup/page.js
@@ -1,7 +1,8 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import Layout from '@/app/components/layout';
 
 const Signup = () => {
 	const router = useRouter();
@@ -75,72 +76,80 @@ const Signup = () => {
 	
 	if (error) {
 		return (
-			<div>
-				<div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
-					<div className="card-body text-center">
-						<div>
-							<p>Email already exists</p>
-							<br />
-							<h2>Login</h2>
-							<p>Sign In to your account</p>
-							<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
-							<span>  </span>
-							<a href="/users/signup" type="button" className="btn btn-secondary active mt-3">Signup</a>
+			<>
+				<Layout>
+					<div>
+						<div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
+							<div className="card-body text-center">
+								<div>
+									<p>Email already exists</p>
+									<br />
+									<h2>Login</h2>
+									<p>Sign In to your account</p>
+									<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
+									<span>  </span>
+									<a href="/users/signup" type="button" className="btn btn-secondary active mt-3">Signup</a>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</Layout>
+			</>
 		);
 	}
 	// You can have them redirected to profile (your choice)
 
 	return (
-		<div className="divStyle">
-			<div className='signupForm'>
-				<form onSubmit={handleSubmit}>
-					<h1>Sign Up</h1>
-					<p className="text-muted">Create an account below to get started</p>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-						<input type="text" className="form-control" placeholder="Full Name" value={fullName} onChange={handleFullName} required />
-					</div>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-						<input type="text" className="form-control" placeholder="Username" value={username} onChange={handleUsername} required />
-					</div>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-mail-forward" aria-hidden="true"></i></span>
-						<input type="email" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required />
-					</div>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-lock"></i></span>
-						<input type="password" className="form-control" placeholder="Password" value={password} onChange={handlePassword} required />
-					</div>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-						<input type="date" className="form-control" placeholder="Birthdate" value={birthdate} onChange={handleBirthdate} />
-					</div>
-					<div className="input-group mb-3">
-						<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-						<input type="text" className="form-control" placeholder="Location" value={location} onChange={handleLocation} />
-					</div>
-					<div className="row">
-						<div className="col-6">
-							<button type="submit" className="btn btn-primary px-4">Sign Up</button>
+		<>
+			<Layout>
+				<div className="column">
+					<div className='signupForm'>
+						<form onSubmit={handleSubmit}>
+							<h1>Sign Up</h1>
+							<p className="text-muted">Create an account below to get started</p>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
+								<input type="text" className="form-control" placeholder="Full Name" value={fullName} onChange={handleFullName} required />
+							</div>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
+								<input type="text" className="form-control" placeholder="Username" value={username} onChange={handleUsername} required />
+							</div>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-mail-forward" aria-hidden="true"></i></span>
+								<input type="email" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required />
+							</div>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-lock"></i></span>
+								<input type="password" className="form-control" placeholder="Password" value={password} onChange={handlePassword} required />
+							</div>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
+								<input type="date" className="form-control" placeholder="Birthdate" value={birthdate} onChange={handleBirthdate} />
+							</div>
+							<div className="input-group mb-3">
+								<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
+								<input type="text" className="form-control" placeholder="Location" value={location} onChange={handleLocation} />
+							</div>
+							<div className="row">
+								<div className="col-6">
+									<button type="submit" className="btn btn-primary px-4">Sign Up</button>
+								</div>
+								<div className="col-6 text-right">
+									<button type="button" className="btn btn-link px-0">Forgot password?</button>
+								</div>
+							</div>
+						</form>
+						<hr />
+						<div>
+							<h2>Already have an account? Click the link below to login.</h2>
+							<p>Sign In to your account</p>
+							<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
 						</div>
-						<div className="col-6 text-right">
-							<button type="button" className="btn btn-link px-0">Forgot password?</button>
-						</div>
 					</div>
-				</form>
-				<hr />
-				<div>
-					<h2>Already have an account? Click the link below to login.</h2>
-					<p>Sign In to your account</p>
-					<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
 				</div>
-			</div>
-		</div>
+			</Layout>
+		</>
 	);
 };
 
