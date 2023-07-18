1d65f085a10bf9515bc4ab4951d10fcf7f732e36
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-07-18 00:14:25 -0400

    feat: created media query for profile page

--- src/app/components/recipes/recipeScrollTile.js
+++ src/app/components/recipes/recipeScrollTile.js
@@ -12,26 +12,26 @@ export default function RecipeScrollTile({ type, number, id }) {
     const [recipesLoading, setRecipesLoading] = useState(true);
 
     let myTile = document.getElementsByClassName('scroll-tile');
-    
+
     useEffect(() => {
         let recipeQuery = `${process.env.NEXT_PUBLIC_SERVER_URL}/recipes/`;
         if (type == 'trending') recipeQuery = recipeQuery.concat(`trending/${number}`);
         if (type == 'my') recipeQuery = recipeQuery.concat(`my`);
         if (type == 'favorite') recipeQuery = recipeQuery.concat(`favorite`);
         axios.get(recipeQuery)
-        .then((data) => {
-            setRecipes(data.data.recipes);
-            setRecipesLoading(false);
-        });
+            .then((data) => {
+                setRecipes(data.data.recipes);
+                setRecipesLoading(false);
+            });
     }, [number, type])
 
-    
 
 
-   
-    
+
+
+
     const renderScrollTile = () => {
-        if(recipes) {
+        if (recipes) {
             const recipeTiles = recipes.map((recipe, i) => (
                 <RecipePreviewContainer key={recipe._id} recipe={recipe} />
             ));
@@ -57,16 +57,16 @@ export default function RecipeScrollTile({ type, number, id }) {
 
 
             <div className={type}>
-          {type === 'favorite' ? <h2 className='title is-1'>Favorite Recipes</h2> : null}
-            {type === 'trending' ? <h2 className='title is-1 trending-title'>What's shakin</h2> : null}
-            {type === 'my' ? <h2 className='title is-1 my-title'>My Bar</h2> : null}
+                {type === 'favorite' ? <h2 className='title is-1'>Favorite Recipes</h2> : null}
+                {type === 'trending' ? <h2 className='title is-1 trending-title'>What&apos;s shakin</h2> : null}
+                {type === 'my' ? <h2 className='title is-1 my-title'>My Bar</h2> : null}
                 <div className='tile is-ancestor scroll-tile is-responsive'>
                     <div className='tile is-parent box is-vertical scroll-func'>
-                    
+
 
 
                         {renderScrollTile()}
-                        {!recipes && type==='my'? <p className='subtitle'>Your bar is looking a little empty...</p> : null}
+                        {!recipes && type === 'my' ? <p className='subtitle'>Your bar is looking a little empty...</p> : null}
                     </div>
                 </div>
             </div>
